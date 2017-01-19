098c719d913cac59433dd189e887ea7bfff9a054
Chris Parolini <chris.parolini@chatid.com>
2017-01-19 13:17:33 -0600

    adding twitter function

--- liri.js
+++ liri.js
@@ -34,3 +34,26 @@ switch (userInput) {
     default:
         console.log("Enter 'myTwitter', 'spotifyThisSong', 'movieThis', or 'Other'");
 }
+
+//This is the function to be called when user inputs node app.js tweets.  This function will pull 20 twitter posts and print them in the terminal
+function twitterFunction() {
+    //for user based authentication.  Var client now holds the twitter keys that are stored in keys.js
+    var client = new twitter(twitterKeys);
+    //passing the endpoint and parameters.
+    client.get("statuses/user_timeline", {
+            count: 20
+        }, function(error, tweets, response) {
+            if (!error)
+            //object"tweets".methodName"forEach"--->using forEach method to execute the function
+                tweets.forEach(function(tweet) {
+                //console.logging tweet text, creation time, times favorited, times retweeted
+                console.log(tweet.text);
+                console.log("Created: " + tweet.created_at);
+                console.log("Favorited: " + tweet.favorite_count + " times");
+                console.log("Retweeted: " + tweet.retweet_count + " times");
+                console.log("***********************************");
+            });
+        })
+        //the user input will append to the log.txt file(ex. my tweets)
+    fs.appendFile("log.txt", ", " + userInput);
+};
